8681868a00f642f27d94335a48981b4c4c002915
Justin Dosaj <jdosaj@vurge.io>
2024-08-01 19:44:52 -0700

    Branch Commit

--- src/pages/news/post/[slug].tsx
+++ src/pages/news/post/[slug].tsx
@@ -17,6 +17,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const Post: React.FC = ({slug}: any) => {
 
+
+    {/*Does this change things */}
     return (
         <main className={`bg-gray-900 flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
             <div className="my-auto mx-auto text-white text-center">
