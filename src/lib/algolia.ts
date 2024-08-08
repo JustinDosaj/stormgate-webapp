eb31f3bf93cc85df36ca6a961032a3bd9c67bd1f
Justin Dosaj <jdosaj@vurge.io>
2024-08-07 18:39:08 -0700

    Update

--- src/lib/algolia.ts
+++ src/lib/algolia.ts
@@ -2,7 +2,7 @@
 import algoliasearch, { SearchIndex } from 'algoliasearch/lite';
 
 const ALGOLIA_APP_ID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
-const ALGOLIA_SEARCH_KEY = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY;
+const ALGOLIA_SEARCH_KEY = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_KEY;
 const ALGOLIA_INDEX_NAME = `${process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME}`;
 
 const searchClient = algoliasearch(ALGOLIA_APP_ID!, ALGOLIA_SEARCH_KEY!);
