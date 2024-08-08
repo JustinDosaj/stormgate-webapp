605669ca6af2a07b8a5e6a579ca4937a0081add3
Justin Dosaj <jdosaj@vurge.io>
2024-08-08 14:00:06 -0700

    Fixed Blog and google button

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -101,21 +101,6 @@ export default function Login() {
                   onClick={login}
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                 >
-                  <svg
-                    className="h-5 w-5 mr-2"
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fab"
-                    data-icon="google"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 488 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M488 261.8c0 141.2-97.2 240.2-240.1 240.2-47.9 0-91.4-14-128.3-37.9l66-66.5c19.9 13.4 45.4 21.5 72.3 21.5 62.4 0 115.7-40.8 134.5-97.6H244.3V261.8h243.7zm0-40H244.3v-86h-65.6v86h-86v65.6h86v86h65.6v-86h185.2c-1.5 16.4-6.5 32.4-15.2 46.7l66.5 65.9C462 310.2 488 287.8 488 261.8z"
-                    ></path>
-                  </svg>
                   Sign in with Google
                 </button>
               </div>
