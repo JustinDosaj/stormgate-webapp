a9fc9f11564a7ae90c3e8e0cda3ad6721cc4b008
Justin Dosaj <jdosaj@vurge.io>
2024-08-07 19:15:40 -0700

    Coming Soon News

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -8,8 +8,8 @@ export default function Builds() {
   return (
     <main className={`bg-gray-900 flex min-h-screen flex-col items-center justify-between p-24 ${inter}`}>
         <div className="mx-auto my-auto text-center text-white">
-        <div>Explore News Page</div>
-            <Link href={{pathname: `/news/post/news-article-1`}}>Go to Articles</Link>
+        <div>News & Updates Coming Soon</div>
+            {/*<Link href={{pathname: `/news/post/news-article-1`}}>Go to Articles</Link>*/}
         </div>
     </main>
   );
