14edd17999a237718b3de7996f91ea16e7d8a051
Justin Dosaj <jdosaj@vurge.io>
2024-08-08 13:32:07 -0700

    Finalize blog
    Still need json ld update

--- src/pages/explore/post/[slug].tsx
+++ src/pages/explore/post/[slug].tsx
@@ -6,6 +6,7 @@ import { Container } from '@/components/shared/container';
 import { getEntriesForContentTypes } from "@/lib/contentful";
 import { RecentPostCard } from "@/components/ui/explore/recentPostCard";
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,48 +35,72 @@ export const getServerSideProps: GetServerSideProps = async ( context) => {
 
 const Post: React.FC = ({ post, url, recentPosts }: any) => {
   
-    const { title, image, author, contentBlocks, category, ogDescription, seoDescription, seoTitle, slug, summary,  } = post.fields;
+    const { title, image, author, contentBlocks, category, ogDescription, seoDescription, seoTitle, slug, summary, logo } = post.fields;
     const { createdAt } = post.sys 
 
-    return (
-        <main className={`bg-gray-900 flex min-h-screen py-24 ${inter.className}`}>
-        <Container className="flex flex-col lg:flex-row justify-between items-start gap-8">
-            <div className="lg:w-2/3 text-white">
-                <h1 className="text-4xl font-bold text-white mb-2">{title}</h1>
-                <div className="text-gray-500 text-sm mb-4">
-                    {new Date(createdAt).toLocaleDateString()} by {author}
-                </div>
-                <div className="mb-8">
-                    <p>{summary}</p>
-                </div>
-                {image && (
-                    <img
-                    src={image.fields.file.url}
-                    alt={title}
-                    className="mb-8 rounded-lg w-full object-scale-down border"
-                    />
-                )}
-                <div className="text-white space-y-4">
-                    {contentBlocks.map((block: any) =>
-                        <div key={block.sys.id}>
-                            {renderContentBlock(block)}
-                        </div>
-                    )}
-                </div>
-            </div>
+    const imageUrl = image.fields.file.url;
+    const absoluteImageUrl = imageUrl.startsWith('//') ? `https:${imageUrl}` : imageUrl;
+    
+    const logoUrl = logo.fields.file.url;
+    const absoluteLogoUrl = logoUrl.startsWith('//') ? `https:${logoUrl}` : logoUrl; // This is used to schema if there is one
 
-            <aside className="lg:w-1/3 lg:sticky lg:top-24">
-            <h2 className="text-2xl font-bold text-white mb-4">Recent Posts</h2>
-            {recentPosts.blogPost.map((post: any) => (
-                <>
-                <RecentPostCard key={post.slug} post={post}/>
-                <RecentPostCard key={post.slug} post={post}/>
-                <RecentPostCard key={post.slug} post={post}/>
-                </>
-            ))}
-            </aside>
-        </Container>
+    return (
+        <>
+        <Head>
+        <title>{seoTitle}</title>
+          <meta name="title" content={seoTitle}/>
+          <meta name="description" content={seoDescription}/>
+          <meta property="og:title" content={seoTitle}/>
+          <meta property="og:description" content={ogDescription}/>
+          <meta property="og:image" content={absoluteImageUrl}/>
+          <meta property="og:url" content={url}/>
+          <meta property="twitter:image" content={absoluteImageUrl}/>
+          <meta property="twitter:title" content={seoTitle}/>
+          <meta property="twitter:description" content={seoDescription}/>
+          {/*<script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+          />*/}
+        </Head>
+        <main className={`bg-gray-900 flex min-h-screen py-24 ${inter.className}`}>
+            <Container className="flex flex-col lg:flex-row justify-between items-start gap-8">
+                <div className="lg:w-2/3 text-white">
+                    <h1 className="text-4xl font-bold text-white mb-2">{title}</h1>
+                    <div className="text-gray-500 text-sm mb-4">
+                        {new Date(createdAt).toLocaleDateString()} by {author}
+                    </div>
+                    <div className="mb-8">
+                        <p>{summary}</p>
+                    </div>
+                    {image && (
+                        <img
+                        src={image.fields.file.url}
+                        alt={title}
+                        className="mb-8 rounded-lg w-full object-scale-down border"
+                        />
+                    )}
+                    <div className="text-white space-y-4">
+                        {contentBlocks.map((block: any) =>
+                            <div key={block.sys.id}>
+                                {renderContentBlock(block)}
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <aside className="lg:w-1/3 lg:sticky lg:top-24">
+                <h2 className="text-2xl font-bold text-white mb-4">Recent Posts</h2>
+                {recentPosts.blogPost.map((post: any) => (
+                    <>
+                    <RecentPostCard key={post.slug} post={post}/>
+                    <RecentPostCard key={post.slug} post={post}/>
+                    <RecentPostCard key={post.slug} post={post}/>
+                    </>
+                ))}
+                </aside>
+            </Container>
         </main>
+        </>
     );
 };
 
