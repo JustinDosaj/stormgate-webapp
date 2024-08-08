f9c284ec9662c1caa7c8dc901ea11e9ecc3b656b
Justin Dosaj <jdosaj@vurge.io>
2024-08-07 18:20:28 -0700

    Security Update

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -3,7 +3,7 @@ import { createClient } from "contentful";
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN as string,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
 });
 
 export const fetchEntries = async (contentType: string) => {
