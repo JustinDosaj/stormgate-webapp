16fc434dce08026d1de4f060aea79e8f92e411cb
Justin Dosaj <jdosaj@vurge.io>
2024-08-09 20:47:20 -0700

    Update
    1. Added explore list to front page
    2. Fixed mobile navigation to profile
    3. Updated title text across web pages

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -5,46 +5,15 @@ import { getEntriesForContentTypes } from '@/lib/contentful';
 import { Container } from '@/components/shared/container';
 import { PostCard } from '@/components/ui/explore/card';
 import { useState } from 'react';
+import { ExploreList } from '@/components/ui/explore/explore-list';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const Explore: React.FC = ({ blogContent }: any) => {
-  const router = useRouter();
-
-  const handlePageChange = (page: number) => {
-    router.push(`/explore?page=${page}`);
-  };
-
-  const [ currentPage, setCurrentPage ] = useState(1);
-
-  const sortedBlogContent = blogContent.sort((a: any, b: any) => new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime());
 
   return (
     <main className={`bg-gray-900 flex min-h-screen flex-col items-center justify-between py-24 ${inter.className}`}>
-      <Container className="container mx-auto">
-        <h1 className="text-4xl font-bold text-white mb-8">News, Guides & More</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {sortedBlogContent.map((post: any) => (
-            <PostCard key={post.slug} post={post}/>
-          ))}
-
-        </div>
-
-        {/* Pagination */}
-        <div className="flex justify-center mt-6">
-          {Array.from({ length: 1 }, (_, index) => (
-            <button
-              key={index}
-              onClick={() => handlePageChange(index + 1)}
-              className={`mx-1 px-3 py-1 rounded-md ${
-                currentPage === index + 1 ? 'bg-violet-700 text-white' : 'bg-gray-800 text-gray-300 hover:bg-violet-700'
-              }`}
-            >
-              {index + 1}
-            </button>
-          ))}
-        </div>
-      </Container>
+      <ExploreList blogContent={blogContent}/>
     </main>
   );
 };
@@ -53,9 +22,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   const entries = await getEntriesForContentTypes(['blogPost'])
 
-  console.log(entries)
   const blogContent = entries.blogPost
-
 
   return {
     props: { blogContent },
